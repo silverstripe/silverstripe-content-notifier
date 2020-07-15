aeffef0028584229991aac14e36fed2db48a96bf
Sam Minnee <sam@silverstripe.com>
2020-07-15 13:19:22 +1200

    FIX: SS4-compatability fixes.

--- src/Model/ContentNotifierEmail.php
+++ src/Model/ContentNotifierEmail.php
@@ -6,7 +6,6 @@ use SilverStripe\ContentNotifier\Extensions\ContentNotifierExtension;
 use SilverStripe\Control\Controller;
 use SilverStripe\Control\Director;
 use SilverStripe\Control\Email\Email;
-use SilverStripe\Core\Object;
 use SilverStripe\ORM\DataList;
 use SilverStripe\ORM\GroupedList;
 
@@ -33,7 +32,7 @@ class ContentNotifierEmail
         $this->emailer->setFrom($config->from);
         $this->emailer->setTo($config->to);
         $this->emailer->setSubject($config->subject);
-        $this->emailer->setTemplate($config->template);
+        $this->emailer->setHTMLTemplate($config->template);
     }
     /**
      * @param DataList $list
@@ -52,7 +51,7 @@ class ContentNotifierEmail
         ContentNotifierExtension::disable_filtering();
         $total = $this->records->count();
         $grouped = GroupedList::create($this->records->limit($this->config()->items_limit))->GroupedBy('Category');
-        $this->emailer->populateTemplate(array('Headline' => $this->config()->headline, 'GroupedItems' => $grouped, 'Total' => $total, 'Link' => Controller::join_links(Director::absoluteBaseURL(), 'admin', 'content-notifications')));
+        $this->emailer->setData(array('Headline' => $this->config()->headline, 'GroupedItems' => $grouped, 'Total' => $total, 'Link' => Controller::join_links(Director::absoluteBaseURL(), 'admin', 'content-notifications')));
         $this->emailer->send();
         foreach ($this->records as $record) {
             $record->HasNotified = true;
